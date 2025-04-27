00c221bbd4d3c2aafd3095d9c04e220bf13c5fc4
hafizasad419 <hafizasad419@gmail.com>
2025-04-27 18:13:12 +0500

    update server url

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:5000/api/v1"
+// export const BASE_URL = "http://localhost:5000/api/v1"
+export const BASE_URL = "https://lender-app-backend.vercel.app"
 
 
 // export const LENDER_APP_ACCESS = "lender-app-access"
