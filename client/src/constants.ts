b6338aefa3daf25aa1aa6d31db326ac4cf73efb2
hafizasad419 <hafizasad419@gmail.com>
2025-04-27 18:30:42 +0500

    delete dotenv

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,6 @@
-// export const BASE_URL = "http://localhost:5000/api/v1"
-export const BASE_URL = "https://lender-app-backend.vercel.app/api/v1"
+export const BASE_URL = import.meta.env.VITE_NODE_ENV === 'development'
+  ? 'http://localhost:5000/api/v1'  // For development
+  : 'https://lender-app-backend.vercel.app/api/v1';  // For production
 
 
 // export const LENDER_APP_ACCESS = "lender-app-access"
